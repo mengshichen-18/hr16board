cfe0f9de8cfdb966e52ddfec1e01cf6cd7aa9840
Chen Mengshi <chenmengshi@sjtu.edu.cn>
2025-09-19 16:55:11 +0800

    change head/foot bar

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,10 +5,10 @@ export function Footer() {
         <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
             <p className="text-sm">
                 <Link
-                    href="https://docs.netlify.com/frameworks/next-js/overview/"
+                    href="https://www.sjtu.edu.cn"
                     className="decoration-dashed text-primary underline-offset-8"
                 >
-                    Next.js on Netlify
+                    今天天气不错
                 </Link>
             </p>
         </footer>
